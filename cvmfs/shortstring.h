1d58958088d7445a77038908857de8ce24eda237
Jose Molina <jose.molina@cern.ch>
2015-09-21 08:29:19 +0200

    Added test changes

--- cvmfs/shortstring.h
+++ cvmfs/shortstring.h
@@ -20,8 +20,8 @@ namespace CVMFS_NAMESPACE_GUARD {
 #endif
 
 const unsigned char kDefaultMaxName = 25;
-const unsigned char kDefaultMaxLink = 25;
-const unsigned char kDefaultMaxPath = 200;
+const unsigned char kDefaultMaxLink = 175;
+const unsigned char kDefaultMaxPath = 175;
 
 template<unsigned char StackSize, char Type>
 class ShortString {
