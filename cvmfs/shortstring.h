7aa7e964cb8731b1e6ab1be574a774e6925051bc
Jose Molina <jose.molina@cern.ch>
2015-09-22 17:36:19 +0200

    Link 75

--- cvmfs/shortstring.h
+++ cvmfs/shortstring.h
@@ -20,7 +20,7 @@ namespace CVMFS_NAMESPACE_GUARD {
 #endif
 
 const unsigned char kDefaultMaxName = 25;
-const unsigned char kDefaultMaxLink = 175;
+const unsigned char kDefaultMaxLink = 75;
 const unsigned char kDefaultMaxPath = 200;
 
 template<unsigned char StackSize, char Type>
